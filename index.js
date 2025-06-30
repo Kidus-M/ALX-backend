21009b914cd9f0f20728aee6fbc0b9e8db7a51f7
Kidus Mesfin Teferi <140906551+Kidus-M@users.noreply.github.com>
2025-06-30 08:44:35 +0300

    Update index.js

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ app.get('/', (req, res) => {
   res.send('Hello World from Express!');
 });
 
+
+
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
